e0c5531b2f44a4211593b19c99624979d0c17afd
GregorioF <gregorio.freidin@gmail.com>
2017-09-23 18:18:38 -0300

    ejercicio 4 hecho

---
+++ 04-Portfolio/C#/Patterns-Portfolio-Exercise-WithAccountImplementation/Portfolio.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patterns_Portfolio_Exercise_WithAccountImplementation
+{
+    class Portfolio: SummarizingAccount
+    {
+
+    	public static String ACCOUNT_NOT_MANAGED = "No se maneja esta cuenta";
+	    public static String ACCOUNT_ALREADY_MANAGED = "La cuenta ya estÃ¡ manejada por otro portfolio";
+
+        private List<SummarizingAccount> m_accounts ;
+	
+	    public static Portfolio createWith(SummarizingAccount anAccount1, SummarizingAccount anAccount2) {
+            List<SummarizingAccount> accounts = new List<SummarizingAccount>() { anAccount1, anAccount2 };
+            return Portfolio.createWith(accounts);
+	    }
+
+    	public static Portfolio createWith(List<SummarizingAccount> summarizingAccounts) {
+            bool hayDuplicados = false;
+            summarizingAccounts.ForEach( account => hayDuplicados = hayDuplicados || summarizingAccounts.Where(a => a.manages(account)).ToList().Count > 1);
+
+            if (hayDuplicados) throw new Exception(ACCOUNT_ALREADY_MANAGED);
+
+
+
+            Portfolio res = new Portfolio();
+            res.m_accounts = new List<SummarizingAccount>(summarizingAccounts);
+            return res;
+        }
+	
+    	public double balance() {
+            return m_accounts.Sum(account => account.balance());
+	    }
+	
+	    public bool registers(AccountTransaction transaction) {
+            bool res = false;
+            m_accounts.ForEach(acount => res = acount.registers(transaction) || res);
+            return res;
+    	}
+
+        public bool manages(SummarizingAccount account)
+        {
+            if (account == this) return true;
+
+            bool res = false;
+            m_accounts.ForEach(acount => res = acount.manages(account) || res);
+            return res;
+        }
+
+        public List<AccountTransaction> transactionsOf(SummarizingAccount account) {
+            if( !manages(account))
+            {
+                throw new Exception(ACCOUNT_NOT_MANAGED);
+            }
+            return account.transactions();
+	    }
+	
+	
+	    public List<AccountTransaction> transactions() {
+            List<AccountTransaction> res = new List<AccountTransaction>();
+            m_accounts.ForEach(account => res.AddRange(account.transactions()));
+            return res;
+    	}
+    }
+}
